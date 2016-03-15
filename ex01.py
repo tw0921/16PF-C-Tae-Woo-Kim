03d7de3fb26bab1ec41d74126bd28c81afafadfa
김태우 <tw0921@naver.com>
2016-03-15 13:00:54 +0900

    ex01.py 내용추가

--- ex01.py
+++ ex01.py
@@ -1,1 +1,7 @@
 print "Hello World"
+print "Helo Again"
+print "I like typing this,"
+print 'This is fun.'
+print "Yay! printing."
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this,'
