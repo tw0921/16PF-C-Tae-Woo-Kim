d56b36c566d9d5974732e861d8921a6a49b9ed0a
Tae woo Kim <kangwon@gmail.com>
2016-03-23 20:41:16 +0900

    사칙연산

---
+++ ex03/ex03.py
@@ -0,0 +1,25 @@
+#-*-coding:cp949
+#이제 닭의 수를 셀 것임
+print "I will now count my chickens:"
+#"Hens" = 25.0 + 30.0 / 6.0 "Roosters" = 100.0 - 25.0 * 3.0 % 4.0
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+#이제 계란 수를 셀 것임.
+print "Now I will count the eggs:"
+#계란 수는 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+#3.0 + 2.0 < 5.0 - 7.0 값이 맞니
+print "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+#3.0 + 2.0 < 5.0 - 7.0 의 결과는
+print 3.0 + 2.0 < 5.0 - 7.0
+#3 + 2의 값 출력 5 - 7의 값 출력
+print "What is 3.0 + 2.0?", 3.0 + 2.0
+print "What is 5.0 - 7.0?", 5.0 - 7.0
+#"oh, that's why it's Fals." 출력
+print "Oh, that's why it's False."
+#"How about some more 출력
+print "How about some more."
+#값에 따라 "true", "false" 출력
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
