e846d557406c8cef5a32839fc1375ec208b3e9dd
CAD Client <CAD Client>
2016-04-05 10:50:39 +0900

    ex07.py more printing

---
+++ ex07/ex07.py
@@ -0,0 +1,22 @@
+#-*-coding:cp949
+print "Mary had a little lamb." # 메리는 작은양을가지고 있었다.
+print "Its fleece was white as %s." % 'snow' #그 양의 털색은 눈색이었다.
+print "And everywhere that Mary went." #그리고 모든 곳에서 마리아는갔다
+print "." * 10 # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+print end1 + end2 + end3 + end4 + end5 + end6, #Cheese
+print end7 + end8 + end9 + end10 + end11 + end12 #Burger
