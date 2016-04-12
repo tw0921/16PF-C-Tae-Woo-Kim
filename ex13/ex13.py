3c743946587fdf1f45ddbb01563781599f43c6a9
CAD Client <CAD Client>
2016-04-12 12:22:58 +0900

    ex13.py 실행방법3, 4 비교

---
+++ ex13/ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+
+print("argb = " + str(argv))
+script, first, second, third = argv
+
+print ("The script is called: %s" % script)
+print ("Your first variable is: %s" % first)
+print ("Your second variable is: %s" % second)
+print ("Your third variable is: %s" % third)
+
