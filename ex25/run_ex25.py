89dbdc5f28767467ca5d492d37105968da4d0408
CAD Client <CAD Client>
2016-05-14 10:07:36 +0900

    entered ex25.py contents

---
+++ ex25/run_ex25.py
@@ -0,0 +1,19 @@
+# -*-coding:utf8
+import ex25
+
+
+sentence = "All good things come to those who wait."
+words = ex25.break_words(sentence)
+print("words = %s" % words)
+sorted_words = ex25.sort_words(words)
+print("sorter_words = %s" % sorted_words)
+ex25.print_first_word(words)
+ex25.print_last_word(words)
+print("words = %s" % words)
+ex25.print_first_word(sorted_words)
+ex25.print_last_word(sorted_words)
+print("sorted_words = %s" % sorted_words)
+sorted_words = ex25.sort_sentence(sentence)
+print("sorted_words = %s" % sorted_words)
+ex25.print_first_and_last(sentence)
+ex25.print_first_and_last_sorted(sentence)
