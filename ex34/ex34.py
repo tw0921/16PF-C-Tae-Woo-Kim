46e80494acf7302c1393427ffedd681d7e8a9438
CAD Client <CAD Client>
2016-05-24 11:42:55 +0900

    ex34.py Accessing Elements of Lists

---
+++ ex34/ex34.py
@@ -0,0 +1,13 @@
+# -*-coding:utf8
+# 참고문헌 : http://learnpythonthehardway.org/book/ex34.html
+
+animals = ['곰', '호랑이', '펭귄', '얼룩말']
+bear =  animals[0]
+tiger =  animals[1]
+penguin = animals[2]
+zebra = animals[3]
+
+print("bear = %s" % bear)
+print("tiger = %s" % tiger)
+print("penguin = %s" % penguin)
+print("zebra = %s" % zebra)
